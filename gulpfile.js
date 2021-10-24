395f902fd664c0dc46a789bbe72cdd257234ad6c
Marcelo Cavalcante <mmcant@gmail.com>
2021-10-24 19:53:11 -0300

    adicionar sass

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ const stripCss = require('gulp-strip-css-comments')
 const htmlmin = require('gulp-htmlmin')
 const babel = require('gulp-babel')
 const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')( require('node-sass'))
+const { pipe } = require('stdout-stream')
+const { contains } = require('jquery')
 const reload = browserSync.reload
 
 function tarefasCSS(cb) {
@@ -18,17 +21,25 @@ function tarefasCSS(cb) {
             './node_modules/bootstrap/dist/css/bootstrap.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             './vendor/owl/css/owl.css',
-            './vendor/jquery-ui/jquery-ui.css',
-            './src/css/style.css'
+            './vendor/jquery-ui/jquery-ui.css'
         ])
         .pipe(stripCss())                   // remove comentários css   
-        .pipe(concat('styles.css'))         // mescla arquivos
+        .pipe(concat('libs.css'))           // mescla arquivos
         .pipe(cssmin())                     // minifica css
-        .pipe(rename({ suffix: '.min'}))    // styles.min.css
+        .pipe(rename({ suffix: '.min'}))    // libs.min.css
         .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
 
     cb()
 
+}
+
+function tarefasSASS(cb) {
+    
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sass()) // transforma o sass para css
+        .pipe(gulp.dest('./dist/css')) 
+
+    cb()
 }
 
 function tarefasJS(callback){
@@ -99,10 +110,11 @@ function end(cb){
 }
 
 // series x parallel
-const process = parallel( tarefasHTML, tarefasJS, tarefasCSS, end)
+const process = parallel( tarefasHTML, tarefasJS, tarefasCSS, tarefasSASS, end)
 
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+exports.sass = tarefasSASS
 
 exports.default = process
